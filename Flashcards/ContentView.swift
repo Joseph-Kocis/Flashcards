f27c1e12e195f5b3a94f996e3fbe915153d4a3b7
Jody Kocis <jodykocis@Jodys-iMac.local>
2020-08-28 14:16:32 -0400

    Initial Commit

---
+++ Flashcards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashcards
+//
+//  Created by Jody Kocis on 8/28/20.
+//  Copyright © 2020 JodyKocis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
