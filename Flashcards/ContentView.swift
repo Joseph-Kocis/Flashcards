72eaf4d27eaee40b52b6ffc7e0f5b38c3e25d407
Jody Kocis <jody4422@icloud.com>
2020-08-28 18:39:29 -0400

    Added Card Flip Animation

--- Flashcards/ContentView.swift
+++ Flashcards/ContentView.swift
@@ -9,13 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    //@Environment(\.horizontalSizeClass) var sizeClass
-    
     @State var currentPage = 0
     @State var cards = [
-        CardView(),
-        CardView(),
-        CardView()
+        CardView(front: "Word 1", back: "Definition 1"),
+        CardView(front: "Word 2", back: "Definition 2"),
+        CardView(front: "Word 3", back: "Definition 3")
     ]
     
     var body: some View {
@@ -25,35 +23,84 @@ struct ContentView: View {
                     "Flashcards",
                     displayMode: .inline
                 )
+                .edgesIgnoringSafeArea(.bottom)
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 struct CardView: View {
-    @State var front = "Front"
-    @State var back = "Back"
+    let front: String
+    let back: String
     @State var showsFront = true
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .foregroundColor(.white)
-                .cornerRadius(10)
-                .shadow(color: .black, radius: 5)
-                .padding(.horizontal)
-                .padding(.vertical, 50)
-            Text(showsFront ? front : back)
-            }
-        .onTapGesture {
-            self.showsFront.toggle()
+            CardDataView(
+                front: front,
+                back: back,
+                isFront: true
+            )
+            .rotation3DEffect(.degrees(self.showsFront ? 0.0 : 180.0), axis: (x: 1.0, y: 0.0, z: 0.0))
+            .zIndex(self.showsFront ? 1 : 0)
+            
+            CardDataView(
+                front: front,
+                back: back,
+                isFront: false
+            )
+            .rotation3DEffect(.degrees(self.showsFront ? 180.0 : 0.0), axis: (x: -1.0, y: 0.0, z: 0.0))
+            .zIndex(self.showsFront ? 0 : 1)
         }
-        
+        .onTapGesture {
+            withAnimation(.easeOut) {
+                self.showsFront.toggle()
+            }
+        }
+        .onDisappear {
+            self.showsFront = true
+        }
+    }
+    
+}
+
+struct CardDataView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.colorScheme) var colorScheme
+    
+    let front: String
+    let back: String
+    let isFront: Bool
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(
+                    colorScheme == .light ? Color(UIColor.systemBackground) : Color(UIColor.tertiarySystemBackground)
+                )
+                .cornerRadius(10)
+                .shadow(color: .gray, radius: 5)
+                .padding(.horizontal, horizontalSizeClass == .compact ? 15 : 50)
+                .padding(.vertical, 50)
+            VStack {
+                if !isFront {
+                    Text(front)
+                        .padding(.bottom, 15)
+                    Text(isFront ? front : back)
+                        .foregroundColor(isFront ? Color(UIColor.label) : Color(UIColor.secondaryLabel))
+                } else {
+                    Text(isFront ? front : back)
+                        .foregroundColor(isFront ? Color(UIColor.label) : Color(UIColor.secondaryLabel))
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.colorScheme, .light
+        )
     }
 }
